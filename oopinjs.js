b5208e4be2ecb2ff8a9a825729e7b6c00a1f14d3
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2022-06-10 16:08:53 +0300

    Update oopinjs.js
    Add constructor functions

--- oopinjs.js
+++ oopinjs.js
@@ -1,39 +1,36 @@
-class Animal {
-    constructor(type, name, food) {
+function Animal(type, name, food) {
+    this.type = type;
+    this.name = name;
+    this.food = food;
+
+    this.toString = () => Object.getPrototypeOf(this).constructor.type.name.food;
+    this.sound = function () {
+        return "";
+    }
+
+}
+
+class Cat extends Animal {
+    Cat(type, name, food) {
         this.type = type;
         this.name = name;
         this.food = food;
     }
-    sound() {
-        return "";
-    }
-    toString() {
-        return Object.getPrototypeOf(this).constructor.type;
-        return Object.getPrototypeOf(this).constructor.name;
-        return Object.getPrototypeOf(this).constructor.food;
-
-    }
-}
-
-class Cat extends Animal {
-    constructor(type, name, food) {
-        super(type, name, food);
-    }
-    sound() {
-        return "Meow!"
+    sound = function () {
+        return "Meow!";
     }
 }
 
 class Dog extends Animal {
     constructor(type, name, food) {
         super(type, name, food);
-        }
-    sound() {
-        return "Woof!"
+    }
+    sound = function () {
+        return "Woof!";
     }
 }
 
-const Tom = new Cat("cat", "Tom", "milk");
+let Tom = new Cat("cat", "Tom", "milk");
 console.log(`${Tom.name} is a ${Tom.type}. ${Tom.name} likes to eat ${Tom.food}, and its sound sounds like a ` + Tom.sound());
 const Rex = new Dog("dog", "Rex", "meat");
 console.log(`${Rex.name} is a ${Rex.type}. ${Rex.name} likes to eat ${Rex.food}, and its sound sounds like a ` + Rex.sound());
